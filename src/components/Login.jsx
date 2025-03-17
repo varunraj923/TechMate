9d63fcbdb53a32f256a79c5084d3acd34f869edc
Varun Raj <118544953+varunraj923@users.noreply.github.com>
2025-03-17 20:09:14 +0530

    created Navbar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <h1>Login Page</h1>
+      <h1>Login</h1>
+     
     </div>
+
+   
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-200 shadow-sm">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl ml-7">TechMate</a>
   </div>
-  <div className="flex gap-2">
-    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+  <div className="flex gap-2 mr-10">
+    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto mr-10" />
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
