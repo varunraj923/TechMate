3f3104b26d1672580a803b75a3196b88d7626b47
Varun Raj <118544953+varunraj923@users.noreply.github.com>
2025-03-21 01:07:32 +0530

    login api working successfully with frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 
 import { BrowserRouter,Route, Routes} from 'react-router-dom'
 import Body from './components/Body'
 import Login from './components/Login'
+import appStore from './utils/appStore'
 
 
 const App = () => {
   return (
     <>
+    <Provider store={appStore}>
     <BrowserRouter basename='/'>
     <Routes>
       <Route path='/' element={<Body/>}>
@@ -16,6 +19,8 @@ const App = () => {
       
     </Routes>
     </BrowserRouter>
+
+    </Provider>
     
     </>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginSubmit = async () => {
+    try {
+      const response = await axios.post("http://localhost:7777/login", {
+        emailId, 
+        password
+      }, {withCredentials : true});
+      console.log('Login Successful:', response.data);
+    } catch (err) {
+      console.error('Login Failed:', err.response?.data || err.message);
+      alert('Login failed. Please check your credentials.');
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-     
+    <div className='ml-120 p-8 w-96 bg-white mt-15 shadow-2xl rounded-2xl h-1/2'>
+      <h1 className='text-3xl font-bold text-center'>Login</h1>
+
+      <div className='text-center mt-5'>
+        <div className='email-section mt-4'>
+          <label htmlFor="email" className='text-lg text-left'>Email</label>
+          <br />
+          <input
+            value={emailId}
+            onChange={(e) => setEmailId(e.target.value)}
+            className='border-2 text-center border-gray-500 rounded-md px-7 py-1.5 mt-2'
+            type="text"
+            name="email"
+            placeholder='Enter your email Id'
+          />
+        </div>
+
+        <div className='password-section mt-4'>
+          <label htmlFor="password">Password</label>
+          <br />
+          <input
+            type="password"
+            placeholder='Enter your password'
+            className='border-2 border-gray-500 rounded-md px-7 py-1.5 mt-2'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <button
+        className='px-5 py-1.5 border-2 border-gray-500 bg-purple-800 text-white mt-5 text-center ml-30 rounded-md hover:bg-blue-500'
+        onClick={handleLoginSubmit}
+      >
+        Submit
+      </button>
     </div>
+  );
+};
 
-   
-  )
-}
-
-export default Login
+export default Login;
